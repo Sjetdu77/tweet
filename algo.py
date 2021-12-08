9e5c5213bf05a249599c71055764109758ed51c2
Swider <andy.frot@outlook.fr>
2021-12-08 14:16:41 +0100

    begin machine learning

--- algo.py
+++ algo.py
@@ -1,6 +1,27 @@
-import warnings, pandas as pd, sklearn
+import warnings, pandas as pd, re
+from sklearn.model_selection import train_test_split
 
 warnings.simplefilter('ignore')
 lab = pd.read_csv('./labels.csv')
 
-print(lab)
+rep = lab["tweet"]
+cor = []
+for s in rep:
+    tmp = re.sub(r"[^a-zA-Z0-9 @*]", "", s)
+    f = tmp.find("@")
+    while f > -1:
+        ret = tmp[f:tmp.find(' ', f)]
+        tmp = tmp.replace(ret, '')
+        if tmp[-1] == "@":
+            tmp = tmp[:-1]
+        f = tmp.find("@")
+    cor.append(tmp)
+
+lab["tweet"] = cor
+
+x = lab["tweet"]
+y = lab["class"]
+
+x_train, x_test, y_train, y_test = train_test_split(x, y)
+
+print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
